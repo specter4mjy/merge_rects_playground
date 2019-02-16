a733e17898703b18f0142f27775bf91b059fb008
Jony Ma <majunyang@meican.com>
2019-02-17 01:42:47 +0800

    Added sorted func

--- merg_rects.playground/Contents.swift
+++ merg_rects.playground/Contents.swift
@@ -4,21 +4,67 @@ import UIKit
 import PlaygroundSupport
 
 class MyViewController : UIViewController {
+    let canvas = UIView()
+    
     override func loadView() {
-        let view = UIView()
-        view.backgroundColor = .white
-        view.backgroundColor = .black
-        self.view = view
+        canvas.backgroundColor = .white
+        canvas.backgroundColor = .black
+        self.view = canvas
         
         let rects = [CGRect(x: 30, y: 30, width: 20, height: 20),
                      CGRect(x: 50, y: 30, width: 20, height: 20),
                      CGRect(x: 30, y: 80, width: 20, height: 20),
                      CGRect(x: 30, y: 150, width: 20, height: 20)]
         
+        show(rects: rects, offsetY: 0)
+
+//        let sortedRects = sortWithMinX(rects: rects)
+        let sortedRects = sort(rects: rects, with: .Y)
+        
+        print(sortedRects)
+        
+        return
+    }
+    
+    enum Direction {
+        case X
+        case Y
+    }
+    
+    func sort(rects: [CGRect], with direction: Direction) -> [[CGRect]] {
+        func wantedValue(r: CGRect) -> CGFloat {
+            switch direction {
+            case .X:
+                return r.minX
+            case .Y:
+                return r.minY
+            }
+        }
+        
+        var result = [[CGRect]]()
+    
+        let values = rects.map(wantedValue)
+
+        let uniqueValues = Array(Set(values))
+        let sortedUniqueValues = uniqueValues.sorted()
+        
+        sortedUniqueValues.forEach { v in
+            let rectsWithV = rects.filter { wantedValue(r: $0) == v }
+            result.append(rectsWithV)
+        }
+        
+        return result
+    }
+    
+    func show(rects: [CGRect], offsetY: CGFloat) {
+        var hue: CGFloat = 0
         rects.forEach { rect in
-            let v = UIView(frame: rect)
-            v.backgroundColor = UIColor(hue: CGFloat.random(in: 0..<1), saturation: 1, brightness: 1, alpha: 1)
-            view.addSubview(v)
+            hue = hue + 0.2
+            
+            let r = rect.offsetBy(dx: 0, dy: offsetY)
+            let v = UIView(frame: r)
+            v.backgroundColor = UIColor(hue: hue, saturation: 0.9, brightness: 0.9, alpha: 1)
+            canvas.addSubview(v)
         }
     }
 }
